bda7ecd99bfc8e6bd9634d27abe5fa4a195bf2e5
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-03-19 18:27:48 +0000

    save all changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames.al-sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames.al-sharoa
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const streetname = document.getElementById("street-name").value
+  const streetnumber = parseInt(document.getElementById("age-entered").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your info is: " + street+ ", number " + adrees + "."
+}
